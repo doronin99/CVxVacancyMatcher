2c8daea978bbe6c5b4834b1b113638feb71b9434
Ilya Goldobin <yxtagold.iliy@gmail.com>
2023-12-28 07:38:46 +0300

    add processing time calculation and output

--- web_app/app.py
+++ web_app/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import time
 
 
 def get_cv(vacancy_description):
@@ -34,15 +35,21 @@ def main():
     if cv_or_vac == "Резюме":
         vacancy_description = st.text_area("Введите вашу вакансию:")
         if st.button("Подобрать резюме"):
+            time_s = time.time()
             result = get_cv(vacancy_description)
+            time_f = time.time()
             if result is not None:
                 st.text(result)
+                st.success(f'Обработка заняла {time_f - time_s} секунд')
     elif cv_or_vac == "Вакансия":
         resume_description = st.text_area("Введите ваше резюме:")
         if st.button("Подобрать вакансию"):
+            time_s = time.time()
             result = get_vaccancy(resume_description)
+            time_f = time.time()
             if result is not None:
                 st.text(result)
+                st.success(f'Обработка заняла {time_f - time_s} секунд')
     else:
         st.text("Выберите, что вы ищите")
 
